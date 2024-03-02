82855c2e36c294751adf6f43deb95e2c64f63948
Zyoruk <ce.zyoruk@gmail.com>
2024-03-02 15:27:21 -0600

    Add ObservableGraph and update Observer

--- src/app/shared/classes/Observer.ts
+++ src/app/shared/classes/Observer.ts
@@ -1,8 +1,32 @@
 export type ObservedDataEvent = {
-    type: 'swap' | 'compare' | 'set',
-    indices: number[]
-}
+  type: "swap" | "compare" | "set";
+  indices: number[];
+};
 
-export abstract class Observer<T>  {
-    abstract update(data: T, event: ObservedDataEvent): void;
+type ObserverDataEventAdd = {
+  type: "add";
+  nodes: number[];
+};
+
+type ObserverDataEventRemove = {
+  type: "remove";
+  nodes: number[];
+};
+
+type ObserverDataEventGet = {
+  type: "get";
+  node: number;
+  edges: number[];
+};
+
+export type ObservedDataEventGraph =
+  | ObserverDataEventAdd
+  | ObserverDataEventRemove
+  | ObserverDataEventGet;
+
+export abstract class Observer<T> {
+  abstract update(
+    data: T,
+    event: ObservedDataEvent | ObservedDataEventGraph
+  ): void;
 }
