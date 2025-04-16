c3a3689afd5715cb76650d44e6d8b9ca3fe9bbe6
Luis Simon <lsimon@godaddy.com>
2025-04-16 16:55:32 -0600

    refactor: simplify package scripts and remove unused route generation scripts

--- src/app/routes.ts
+++
@@ -1,3 +0,0 @@
-type Route = { path: string;name: string; };
- const routes: Route[] = [{"path":"/algorithms/searching/binary-search","name":"/algorithms/searching/binary-search"},{"path":"/algorithms/searching/binary-tree","name":"/algorithms/searching/binary-tree"},{"path":"/algorithms/sorting/bubble-sort","name":"/algorithms/sorting/bubble-sort"},{"path":"/algorithms/sorting/merge-sort","name":"/algorithms/sorting/merge-sort"},{"path":"/algorithms/sorting/quick-sort","name":"/algorithms/sorting/quick-sort"}];
-export { routes };
